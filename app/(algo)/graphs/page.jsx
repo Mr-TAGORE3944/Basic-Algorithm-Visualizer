990b72691ecb8ca1d1f7245b916db763991d451a
Tagore kommu <kommu1234sharada@gmail.com>
2024-09-26 21:16:37 +0530

    ready to deploy

--- app/(algo)/graphs/page.jsx
+++ app/(algo)/graphs/page.jsx
@@ -20,45 +20,38 @@ class Graph extends Component {
       offset: 0,
     };
   }
-  // setNumber = (event)=>{
-  //     let value = event.target.valueAsNumber;
-  //     if(value!== NaN && value<99)
-  //         this.setState({inputNumber:value});
-  //
-  //     // document
-  // }
+
   setAlgo = (pos, val) => {
     if (pos === 0) {
-      // console.log("sup 0");
       this.setState({ algo: val });
     }
   };
+
   setN = (pos, val) => {
     if (pos === 0) {
-      // console.log("sup 0");
       this.setState({ n: val });
     }
   };
+
   setR = (pos, val) => {
     if (pos === 0) {
-      // console.log("sup 0");
       this.setState({ r: val });
     }
   };
+
   addNumber = () => {
-    // console.log(getFibTree(3));
     let tree = getTree(this.state.n, this.state.algo, this.state.r);
-    this.setState({ edges: [], vertices: [], offset: tree.x });
-    this.state.vertices = [];
-    // this.setState({});
-    this.recur(tree, undefined);
+    this.setState({ edges: [], vertices: [], offset: tree.x }, () => {
+      this.recur(tree, undefined);
+    });
   };
+
   recur = async (node, parent) => {
-    let vertices = this.state.vertices;
-    let current = this.state.vertices.length;
+    let vertices = [...this.state.vertices];
+    let current = vertices.length;
 
     if (parent !== undefined) {
-      if (node.children.length)
+      if (node.children.length) {
         vertices.push({
           label: node.tree.label,
           val: 0,
@@ -67,7 +60,7 @@ class Graph extends Component {
           px: parent.x,
           py: parent.y,
         });
-      else
+      } else {
         vertices.push({
           label: node.tree.label,
           val: node.tree.node,
@@ -76,9 +69,10 @@ class Graph extends Component {
           px: parent.x,
           py: parent.y,
         });
+      }
       this.setState({ vertices, current });
 
-      let edges = this.state.edges;
+      let edges = [...this.state.edges];
       edges.push({
         x1: parent.x,
         y1: parent.y,
@@ -87,7 +81,7 @@ class Graph extends Component {
       });
       this.setState({ edges });
     } else {
-      if (node.children.length)
+      if (node.children.length) {
         vertices.push({
           label: node.tree.label,
           val: 0,
@@ -96,7 +90,7 @@ class Graph extends Component {
           px: node.x,
           py: node.y,
         });
-      else
+      } else {
         vertices.push({
           label: node.tree.label,
           val: node.tree.node,
@@ -105,14 +99,13 @@ class Graph extends Component {
           px: node.x,
           py: node.y,
         });
+      }
       this.setState({ vertices, current });
     }
     await sleep(500);
 
     for (let i = 0; i < node.children.length; i++) {
       await this.recur(node.children[i], node);
-      // let verticess = [...this.state.vertices];
-      // verticess[current].val+=node.children[i].tree.node;
       this.setState({ current });
       await sleep(500);
     }
@@ -120,6 +113,7 @@ class Graph extends Component {
     verticess[current].val = node.tree.node;
     this.setState({ vertices: verticess });
   };
+
   render() {
     return (
       <div className="overflow-auto w-full">
@@ -131,7 +125,6 @@ class Graph extends Component {
           onStart={this.addNumber}
         />
         <Details algo={this.state.algo} />
-
         <CanvasSvg
           vertices={this.state.vertices}
           edges={this.state.edges}
@@ -142,7 +135,9 @@ class Graph extends Component {
     );
   }
 }
+
 function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
+
 export default Graph;
